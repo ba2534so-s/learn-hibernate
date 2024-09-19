c6a48ce4c431d4cfc066932474576332521becf4
Baran <Baran.soysuren@gmail.com>
2024-09-19 23:57:04 +0200

    getConnection

--- Lesson01_JDBC/src/com/learnjdbc/Main.java
+++ Lesson01_JDBC/src/com/learnjdbc/Main.java
@@ -1,10 +1,16 @@
 package com.learnjdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		String sql = "SELECT * FROM students WHERE studentID = 3";
+		
+		Connection connection = DriverManager.getConnection();
+		
 
 	}
 
