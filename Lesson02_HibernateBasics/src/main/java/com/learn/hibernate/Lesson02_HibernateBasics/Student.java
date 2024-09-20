d8b71e5edbb83bd2b7c7eec43128930d9d3c547b
Baran <Baran.soysuren@gmail.com>
2024-09-20 02:25:54 +0200

    add rest of the getters and setters

--- Lesson02_HibernateBasics/src/main/java/com/learn/hibernate/Lesson02_HibernateBasics/Student.java
+++ Lesson02_HibernateBasics/src/main/java/com/learn/hibernate/Lesson02_HibernateBasics/Student.java
@@ -13,5 +13,24 @@ public class Student {
 	public void setStudentId(int studentId) {
 		this.studentId = studentId;
 	}
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getGrade() {
+		return grade;
+	}
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}	
+	
+	
 }
