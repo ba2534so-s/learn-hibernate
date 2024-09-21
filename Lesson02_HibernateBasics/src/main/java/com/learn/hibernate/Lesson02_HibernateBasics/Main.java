70e85812ee886ce5abe4bd7a9c22f17154726130
Baran <Baran.soysuren@gmail.com>
2024-09-22 00:13:48 +0200

    set first name

--- Lesson02_HibernateBasics/src/main/java/com/learn/hibernate/Lesson02_HibernateBasics/Main.java
+++ Lesson02_HibernateBasics/src/main/java/com/learn/hibernate/Lesson02_HibernateBasics/Main.java
@@ -16,7 +16,9 @@ public class Main
     public static void main( String[] args )
     {
     	StudentName name = new StudentName();
-    	
+    	name.setfName("Clark");
+
+
     	
     	Student s = new Student();
     	s.setStudentId(3);
