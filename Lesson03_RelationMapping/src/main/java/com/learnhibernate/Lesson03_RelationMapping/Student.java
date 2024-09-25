bc2ce51abcb7a3e410967e55314a4edbff8a49b1
Baran <Baran.soysuren@gmail.com>
2024-09-25 23:51:26 +0200

    add getters and setters

--- Lesson03_RelationMapping/src/main/java/com/learnhibernate/Lesson03_RelationMapping/Student.java
+++ Lesson03_RelationMapping/src/main/java/com/learnhibernate/Lesson03_RelationMapping/Student.java
@@ -5,5 +5,25 @@ public class Student {
 	private int studentID;
 	private String name;
 	private char grade;
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGrade() {
+		return grade;
+	}
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+	
+	
 
 }
