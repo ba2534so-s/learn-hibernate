27fa2f40e15abd9432e652e003fe4aa2b0421898
Baran <Baran.soysuren@gmail.com>
2024-09-25 23:58:22 +0200

    generate getters and setters

--- Lesson03_RelationMapping/src/main/java/com/learnhibernate/Lesson03_RelationMapping/Laptop.java
+++ Lesson03_RelationMapping/src/main/java/com/learnhibernate/Lesson03_RelationMapping/Laptop.java
@@ -4,5 +4,21 @@ public class Laptop {
 	
 	private int laptopID;
 	private String model;
+	
+	
+	public int getLaptopID() {
+		return laptopID;
+	}
+	public void setLaptopID(int laptopID) {
+		this.laptopID = laptopID;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
 
 }
