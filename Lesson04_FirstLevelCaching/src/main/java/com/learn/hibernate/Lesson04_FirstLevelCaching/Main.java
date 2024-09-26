08d1ee10c8e7b2d2944d97b8f3a65dbf104ed31e
Baran <Baran.soysuren@gmail.com>
2024-09-26 23:48:01 +0200

    add Service Registry

--- Lesson04_FirstLevelCaching/src/main/java/com/learn/hibernate/Lesson04_FirstLevelCaching/Main.java
+++ Lesson04_FirstLevelCaching/src/main/java/com/learn/hibernate/Lesson04_FirstLevelCaching/Main.java
@@ -1,6 +1,8 @@
 package com.learn.hibernate.Lesson04_FirstLevelCaching;
 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hello world!
@@ -11,6 +13,7 @@ public class Main
     public static void main( String[] args )
     {
         Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
         
     }
 }
