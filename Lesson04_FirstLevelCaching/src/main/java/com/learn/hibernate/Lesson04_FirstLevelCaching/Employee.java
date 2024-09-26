2659904e1b94eb75286c27cf9cff3d8a01a9ee1b
Baran <Baran.soysuren@gmail.com>
2024-09-26 23:44:18 +0200

    add id annotation

--- Lesson04_FirstLevelCaching/src/main/java/com/learn/hibernate/Lesson04_FirstLevelCaching/Employee.java
+++ Lesson04_FirstLevelCaching/src/main/java/com/learn/hibernate/Lesson04_FirstLevelCaching/Employee.java
@@ -1,11 +1,12 @@
 package com.learn.hibernate.Lesson04_FirstLevelCaching;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 	
-	
+	@Id
 	private int employeeId;
 	private String name;
 	private String role;
