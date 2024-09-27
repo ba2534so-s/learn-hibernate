dc8eac4fa883397caafe567a451322e48193a69e
Baran <Baran.soysuren@gmail.com>
2024-09-27 17:15:12 +0200

    create employee variable

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
@@ -4,10 +4,11 @@ package com.learn.hibernate.Lesson05_SecondLevelCaching;
  * Hello world!
  *
  */
-public class Main 
+public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Employee e = null;
+        
     }
 }
