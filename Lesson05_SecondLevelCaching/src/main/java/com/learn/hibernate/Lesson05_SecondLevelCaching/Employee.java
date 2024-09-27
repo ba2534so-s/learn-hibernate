e0e732380f3e33b5c7481c9170b23814064a2d3d
Baran <Baran.soysuren@gmail.com>
2024-09-27 17:13:21 +0200

    add getters and setters

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
@@ -10,5 +10,28 @@ public class Employee {
 	private int employeeId;
 	private String name;
 	private String role;
+	
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
 
 }
