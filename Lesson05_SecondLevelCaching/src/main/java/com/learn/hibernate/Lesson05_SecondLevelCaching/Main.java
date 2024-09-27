de9d5924820d79b8cbab7591dcdb2b84ba924016
Baran <Baran.soysuren@gmail.com>
2024-09-27 17:16:08 +0200

    add configuration

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
@@ -1,5 +1,7 @@
 package com.learn.hibernate.Lesson05_SecondLevelCaching;
 
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,9 @@ public class Main
     public static void main( String[] args )
     {
         Employee e = null;
+        
+        Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
+        
         
     }
 }
