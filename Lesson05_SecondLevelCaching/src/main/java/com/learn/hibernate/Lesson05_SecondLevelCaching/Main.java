52af6ac6cd36043c3b4d2ef88eed799fd5a310e6
Baran <Baran.soysuren@gmail.com>
2024-09-27 17:19:42 +0200

    open session

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Main.java
@@ -1,5 +1,6 @@
 package com.learn.hibernate.Lesson05_SecondLevelCaching;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -18,6 +19,7 @@ public class Main
         Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
         ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
         SessionFactory sf = con.buildSessionFactory(reg);
+        Session session1 = sf.openSession();
         
     }
 }
