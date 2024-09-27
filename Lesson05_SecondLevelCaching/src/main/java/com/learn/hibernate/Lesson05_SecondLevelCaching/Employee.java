2708f0721186f4da05bbf863bab2f9870e628df6
Baran <Baran.soysuren@gmail.com>
2024-09-27 17:12:20 +0200

    add id annotation

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
@@ -1,7 +1,10 @@
 package com.learn.hibernate.Lesson05_SecondLevelCaching;
 
+import jakarta.persistence.Id;
+
 public class Employee {
 	
+	@Id
 	private int employeeId;
 	private String name;
 	private String role;
