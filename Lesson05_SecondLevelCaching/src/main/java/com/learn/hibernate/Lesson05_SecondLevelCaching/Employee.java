8ed4107b5505e70c690b25f226b53dca7384bec1
Baran <Baran.soysuren@gmail.com>
2024-09-30 15:05:07 +0200

    add cache concurrency strategy

--- Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
+++ Lesson05_SecondLevelCaching/src/main/java/com/learn/hibernate/Lesson05_SecondLevelCaching/Employee.java
@@ -1,11 +1,15 @@
 package com.learn.hibernate.Lesson05_SecondLevelCaching;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 import jakarta.persistence.Cacheable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 @Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
 public class Employee {
 	
 	@Id
