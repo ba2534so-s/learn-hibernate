d8897b6b0828fe02ddf73ab574c14c14690a0f35
Baran <Baran.soysuren@gmail.com>
2024-09-30 18:09:05 +0200

    Create maven project for lesson 6

---
+++ Lesson06_HibernateQueryLanguage/src/main/java/com/learn/hibernate/Lesson06_HibernateQueryLanguage/App.java
@@ -0,0 +1,13 @@
+package com.learn.hibernate.Lesson06_HibernateQueryLanguage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
