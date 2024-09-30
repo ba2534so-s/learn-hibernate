01fb914e83dcaa83b6e4566d7e15a46faf45cbf1
Baran <Baran.soysuren@gmail.com>
2024-09-30 20:34:13 +0200

    add table annotation and name table Employee

--- Lesson06_HibernateQueryLanguage/src/main/java/com/learn/hibernate/Lesson06_HibernateQueryLanguage/Employee.java
+++ Lesson06_HibernateQueryLanguage/src/main/java/com/learn/hibernate/Lesson06_HibernateQueryLanguage/Employee.java
@@ -1,8 +1,12 @@
 package com.learn.hibernate.Lesson06_HibernateQueryLanguage;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="Employee")
 public class Employee {
+	
+	
 
 }
