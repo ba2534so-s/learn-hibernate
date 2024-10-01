e2b51a5e7ddb4e4997d1e50052bdc8d3d71aeb82
Baran <Baran.soysuren@gmail.com>
2024-10-01 13:14:57 +0200

    build session factory

--- Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Main.java
+++ Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Main.java
@@ -1,5 +1,6 @@
 package com.learn.hibernate.Lesson07_SQLinHibernate;
 
+import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -14,5 +15,6 @@ public class Main
     {
         Configuration con = new Configuration().configure().addAnnotatedClass(null);
         ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+        SessionFactory sf = con.buildSessionFactory(reg);
     }
 }
