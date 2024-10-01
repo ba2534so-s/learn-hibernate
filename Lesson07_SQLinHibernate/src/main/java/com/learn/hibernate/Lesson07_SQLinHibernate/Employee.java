1308319f633d316e824d25f478d019354864b25a
Baran <Baran.soysuren@gmail.com>
2024-10-01 13:21:13 +0200

    override toString method

--- Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
+++ Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
@@ -33,7 +33,9 @@ public class Employee {
 		this.salary = salary;
 	}
 	
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", name=" + name + ", salary=" + salary + "]";
+	}
 	
-	
-
 }
