dfda707576dfbeb361675394876f540277f01220
Baran <Baran.soysuren@gmail.com>
2024-10-01 17:24:19 +0200

    Create maven project for lesson 09

---
+++ Lesson09_GetVsLoad/src/main/java/com/learn/hibernate/Lesson09_GetVsLoad/App.java
@@ -0,0 +1,13 @@
+package com.learn.hibernate.Lesson09_GetVsLoad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
