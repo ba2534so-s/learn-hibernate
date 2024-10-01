48bb3ec2a91433cf443f089b5da9283a88b36d1b
Baran <Baran.soysuren@gmail.com>
2024-10-01 16:53:19 +0200

    create service registry

--- Lesson08_HibernateObjectStates/src/main/java/com/learn/hibernate/Lesson08_HibernateObjectStates/Main.java
+++ Lesson08_HibernateObjectStates/src/main/java/com/learn/hibernate/Lesson08_HibernateObjectStates/Main.java
@@ -1,6 +1,8 @@
 package com.learn.hibernate.Lesson08_HibernateObjectStates;
 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hello world!
@@ -11,5 +13,6 @@ public class Main
     public static void main( String[] args )
     {
         Configuration con = new Configuration().configure().addAnnotatedClass(Employee.class);
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
     }
 }
