37712b94b38b968abb506785b10ad605c22f4df8
Baran <Baran.soysuren@gmail.com>
2024-10-01 13:19:51 +0200

    add Id annotation

--- Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
+++ Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
@@ -1,12 +1,14 @@
 package com.learn.hibernate.Lesson07_SQLinHibernate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="Employee")
 public class Employee {
 	
+	@Id
 	private int employeeId;
 
 }
