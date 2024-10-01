f91f30dc8d77af6ce2d679761e714edac98b62a0
Baran <Baran.soysuren@gmail.com>
2024-10-01 13:19:01 +0200

    add table annotation

--- Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
+++ Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Employee.java
@@ -1,8 +1,10 @@
 package com.learn.hibernate.Lesson07_SQLinHibernate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="Employee")
 public class Employee {
 
 }
