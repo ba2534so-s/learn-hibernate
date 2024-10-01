31a213a21614d7eb51892f3d84a253840f852423
Baran <Baran.soysuren@gmail.com>
2024-10-01 19:57:10 +0200

    create employee

--- Lesson10_JPA/src/main/java/com/learn/hibernate/Lesson10_JPA/Main.java
+++ Lesson10_JPA/src/main/java/com/learn/hibernate/Lesson10_JPA/Main.java
@@ -12,11 +12,24 @@ public class Main
 {
     public static void main( String[] args )
     {
+    	
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence-unit");
         EntityManager em = emf.createEntityManager();
         
+        // Get employee from db
         Employee e = em.find(Employee.class, 101);
         
         System.out.println(e);
+        
+        
+        // save employee to db
+        
+        e = new Employee();
+        e.setEmployeeId(55);
+        e.setName("Bruce Wayne");
+        e.setSalary(33000);
+        
+        
+        
     }
 }
