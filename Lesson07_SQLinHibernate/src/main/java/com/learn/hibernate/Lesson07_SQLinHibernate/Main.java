fd7e79d21575eb8b62e55efbb41806c96d046c0b
Baran <Baran.soysuren@gmail.com>
2024-10-01 13:14:12 +0200

    create service registry

--- Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Main.java
+++ Lesson07_SQLinHibernate/src/main/java/com/learn/hibernate/Lesson07_SQLinHibernate/Main.java
@@ -1,6 +1,8 @@
 package com.learn.hibernate.Lesson07_SQLinHibernate;
 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hello world!
@@ -11,5 +13,6 @@ public class Main
     public static void main( String[] args )
     {
         Configuration con = new Configuration().configure().addAnnotatedClass(null);
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
     }
 }
